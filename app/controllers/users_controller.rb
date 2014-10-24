50128bfcd8e0bff2cd1c9ef208214d36a761e74f
Jacquimik <j.mikulasev@hotmail.com>
2014-10-24 12:38:25 +1100

    Start on user_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new user_params
+		if
+			@user.save
+			session[:user_id] = @user.user_id
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = @current_user
+	end
+
+	def update
+		user = @current_user
+		user.update user_params
+		redirect_to user
+	end
+
+	def index
+		@users = User.ApplicationController
+	end
+
+	def show
+		@user = User.find params[:id]
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:firstname, :lastname:, :email, :email_confirmation, :password, :password_confirmation)
