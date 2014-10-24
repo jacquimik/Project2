348c23ecde2eacdea7d3f3914cfe6652a43b1fdf
Jacquimik <j.mikulasev@hotmail.com>
2014-10-24 16:55:07 +1100

    WORKING LOGINS WOOHOOO

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,14 @@ class SessionsController < ApplicationController
 
 #where the login form POSTs data
 	def create
-		user = User.where(:firstname => params[:email]).first
+		user = User.where(:email => params[:email]).first
 		if user.present? && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			redirect_to root_path
 		else 
-			redirect_to session_path
+			redirect_to new_session_path
 		end
+
 	end
 
 #logout
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,12 @@ module ApplicationHelper
 	def intellinav
 		nav = ''
 		if @current_user.present?
+				"<div class='welcome'>"
+   	 	nav += "<li>Hello #{ @current_user.firstname }</li> "
+      	"</div>"
       nav += "<li>#{ link_to('Sign Up', new_user_path) }</li>"
       nav += "<li>#{link_to('Edit profile  ', edit_user_path(@current_user))}</li>"        
-      nav += "<li>#{link_to('Sign out ' + @current_user.firstname, login_path, :method => :delete, :data => {:confirm => 'Are you sure?'})}  "
-
+      nav += "<li>#{link_to('Sign out ' + @current_user.firstname, new_session_path, :method => :delete, :data => {:confirm => 'Are you sure?'})}  "
     else 
       nav += "<li>#{link_to('Sign up', new_user_path) }</li>"
       nav += "<li>#{ link_to('Login', new_session_path) }</li>"
