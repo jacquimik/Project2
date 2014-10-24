48c3db6a6bf0cb6e80b7f807811484390a308af1
angelamikulasev <angela.mikulasev@hotmail.com>
2014-10-24 14:35:13 +1100

    added contollers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+u1 = User.create!(:firstname => 'Angela', :lastname => 'Mikulasev,' :email => 'angela@angela.com', :password => 'chicken', :password_confirmation => 'chicken')
+
+p1 = Product.create!(:name => 'jacket', :price => '$29.99', :size => '10', :description => 'demin jacket', :brand => 'bordot', :image => 'http://www.bardot.com.au/files/product/images/403527/9631jb1-denim-front-.jpg')
+
+p2 = Product.create!(:name => 't-shirt', :price => '$19.99', :size => '8', :description => 'green shirt', :brand => 'kookai', :image => 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=64181961')
+
+
+Category.create! name: 'accessories'
+Category.create! name: 'coats & jackets'
+Category.create! name: 'denim'
+Category.create! name: 'designer'
+Category.create! name: 'dresses'
+Category.create! name: 'jumpers'
+Category.create! name: 'jeans'
+Category.create! name: 't-shirts'
+Category.create! name: 'tanks'
+Category.create! name: 'jewellery'
+Category.create! name: 'cardigans'
+Category.create! name: 'jumpsuits'
+Category.create! name: 'shirts'
+Category.create! name: 'shoes'
+Category.create! name: 'shorts'
+Category.create! name: 'skirts'
+Category.create! name: 'swimwear'
+Category.create! name: 'pants'
