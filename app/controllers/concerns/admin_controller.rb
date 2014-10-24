6f1a6a88a3754f9c9e9f2f24dabbad072174a911
Jacquimik <j.mikulasev@hotmail.com>
2014-10-24 23:20:00 +1100

    Started CSS - both nav bars and potential logo

---
+++ app/controllers/concerns/admin_controller.rb
@@ -0,0 +1,45 @@
+module Admin
+	class ProductsController < ApplicationController
+		# skip_before_filter :verify_authenticity_token
+
+		def index
+			@products = Product.all
+		end
+
+		def new 
+			@product = Product.new
+		end
+
+		def create
+			product = Product.create product_params
+			redirect_to [:admin, product]
+		end
+
+		def show
+			@product = Product.find params[:id]
+		end
+
+		def destroy
+			product = Product.find params[:id]
+			product.destroy
+
+			redirect_to admin_products_path
+		end
+
+	  def edit
+	    @product = Product.find params[:id]
+	  end
+
+		def update
+			product = Product.find params[:id]
+			product.update product_params
+			redirect_to [:admin, product]
+		end
+
+		private
+
+		def product_params
+			params.require(:product).permit(:name)
+		end
+	end
+end
