08a88cdd2d19e3a211ddba61d16f904444ee12b5
angelamikulasev <angela.mikulasev@hotmail.com>
2014-10-30 22:54:56 +1100

    debug user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,10 @@ class UsersController < ApplicationController
 	        session[:user_id] = @user.id
 					# Merge in an existing cart if the user has one.
 					if session[:cart_id]
-						cart = Cart.find session[:cart_id]
-						cart.user_id = @user.id
-						cart.save
-					end
+					# 	cart = Cart.find session[:cart_id]
+					# 	cart.user_id = @user.id
+					# 	cart.save
+					# end
 		    	redirect_to root_path
 	      else
 	      	render :new
