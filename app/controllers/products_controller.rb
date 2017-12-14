39e5c093c8651e985ab722cce4b9bf40c974d085
angelamikulasev <angela.mikulasev@hotmail.com>
2017-12-15 04:13:25 +1100

    refactoring logic to only get uniq product ids

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,15 +4,25 @@ class ProductsController < ApplicationController
 			category = Category.where(name: params[:category]).first
  			Product.where(category_id: category.id)
 		else
- 			Product.all
+ 			get_products_by_category_id(get_uniq_product_ids)
 		end
 	end
 
 	def show
-		@product = Product.find params[:id]
+		@product = Product.find params[:id].uniq
 	end
 
 	private
+
+	def get_products_by_category_id(get_uniq_product_ids)
+		get_uniq_product_ids.map do |id|
+			Product.find(id)
+		end
+	end
+
+	def get_uniq_product_ids
+		Product.all.map(&:category_id).uniq
+	end
 
 	def product_params
 		params.require(:product).permit(:name, :description, :price, :brand, :size, :image)
